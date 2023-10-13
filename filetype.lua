caee7199567e61ca8022ff0c0db1f6f37ed04435
Hobo Spaceship <hobdenw@gmail.com>
2023-10-13 22:37:28 +0200

    add comment

--- filetype.lua
+++ filetype.lua
@@ -1,3 +1,6 @@
+-- this is necessary for the markdown treesitter parser to 
+-- identify mdx files as markdown files, as noted in this
+-- article: https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/ 
 vim.filetype.add({
     extension = {
         mdx = 'mdx'

c0d47d7ed04e48f33a74f114662bf109c8fef952
Hobo Spaceship <hobdenw@gmail.com>
2023-11-25 16:53:41 +0100

    remap lsp buffer navigation to go the right way

--- lua/hobo/remap.lua
+++ lua/hobo/remap.lua
@@ -42,8 +42,8 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- LSP stuff
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 

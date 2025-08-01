409f866db18a9a8af5e11a84004a89927b7f6304
Will Hobden <hobdenw@gmail.com>
2025-08-01 11:49:16 +0200

    patch lazy git to fix blank floating terminal

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,10 +1,24 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = "LazyGit",
-  keys = {
-    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  -- No need to add "kdheepak/lazygit.nvim" — we'll launch it ourselves
+  {
+    "akinsho/toggleterm.nvim",
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          local Terminal = require("toggleterm.terminal").Terminal
+          local lazygit = Terminal:new({
+            cmd = "lazygit",
+            hidden = true,
+            direction = "float", -- or "tab", "horizontal", etc.
+            float_opts = {
+              border = "curved",
+            },
+          })
+          lazygit:toggle()
+        end,
+        desc = "LazyGit (ToggleTerm)",
+      },
+    },
   },
 }
